788e7bb9fc832123992abf28c7631c0d5417252f
anaveen7 <naveenpractice7@gmail.com>
2023-07-21 18:40:08 +0530

    This is the commit from Asaian guy

--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -5,6 +5,10 @@ public class SeleniumTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This Selenium Test");
+		
+		System.out.println("This is web automation");
+		System.out.println("This is web automation3");
+
 	}
 
 }
