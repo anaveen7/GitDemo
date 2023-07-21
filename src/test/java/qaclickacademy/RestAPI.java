92b7851fe4ace992ec2152bc6d0fd05d96411569
anaveen7 <naveenpractice7@gmail.com>
2023-07-21 18:24:26 +0530

    This is commit from Asaian guy

--- src/test/java/qaclickacademy/RestAPI.java
+++ src/test/java/qaclickacademy/RestAPI.java
@@ -6,6 +6,11 @@ public class RestAPI {
 		// TODO Auto-generated method stub
 		System.out.println("This is rest API Test");
 		System.out.println("This is PostMan Test");
+		System.out.println("This is PostMan Test2");
+		System.out.println("This is PostMan Test3");
+		System.out.println("This is PostMan Test4");
+
+
 
 	}
 
